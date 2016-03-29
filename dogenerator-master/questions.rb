e36a71188eec3836962f250d58791d91bb5a308d
aileenhuang <yeeruhuang@gmail.com>
2016-03-28 22:59:09 -0400

    changed question lists

--- dogenerator-master/questions.rb
+++ dogenerator-master/questions.rb
@@ -2,24 +2,28 @@ require 'sinatra'
 require 'rubygems'
 require 'pry'
 
-QAPAIRS = {
+question_list = [
     #Both robot and human know
-    "What Disney movie is the song “Make a Man Out of You” from?" => ["mulan"],
-    "How many notes are in an octave?" => ["8", "eight"],
-    "What is the name of Harry Potter’s owl?" => ["hedwig"],
-    "State the name of a yellow mouse pokemon with red cheeks." => ["pikachu"],
-    "What is the longest river in Africa?" => ["nile river", "nile"],
-    "Who wrote the Communist Manifesto?" => ["karl marx", "marx", "frederick engels", "engels"],
+    "What Disney movie is the song “Make a Man Out of You” from?",
+    "How many notes are in an octave?",
+    "What is the name of Harry Potter’s owl?",
+    "State the name of a yellow mouse pokemon with red cheeks.",
+    "What is the longest river in Africa?",
+    "Who wrote the Communist Manifesto?",
     #Human knows; robot does not
-    "What is the powerhouse of the cell?" => ["mitochondria"],
-    "In Greek mythology, who is the god of the sea?" => ["neptune"],
-    "Who, according to Greek mythology, allowed all the evils into the world by opening a box?" => ["pandora"],
-    "What weapon does Luke Skywalker use?" => ["lightsaber"],
-    "What famous fictional detective lived on 221B Baker Street?" => ["sherlock holmes"],
-    "Name a member of the Beatles." => ["ringo starr", "george harrison", "john lennon", "paul mccartney"],
-    #Human does not know; robot doesd
-    "Which emperor of Japan transformed Japan from a feudal state to a capitalist world power?" => ["emperor meiji", "meiji"]
-}
+    "What is the powerhouse of the cell?",
+    "In Greek mythology, who is the god of the sea?",
+    "Who, according to Greek mythology, allowed all the evils into the world by opening a box?",
+    "What weapon does Luke Skywalker use?",
+    "What famous fictional detective lived on 221B Baker Street?",
+    "Name a member of the Beatles.",
+    #Human does not know; robot does
+    "Which emperor of Japan transformed Japan from a feudal state to a capitalist world power?"
+]
+
+answer_list = [
+    "mulan", "8", "hedwig", "pikachu", "nile river", "karl marx", "mitochondria", "neptune", "pandora", "lightsaber", "sherlock holmes", "john lennon", "emperor meiji"
+]
 
 # returns HASH of singular nouns
 # takes nouns-hash and tagged string and
