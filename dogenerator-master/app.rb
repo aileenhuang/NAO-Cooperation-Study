e1080f8656183f6b04737e1add6ac8c95a31369c
Aleksandra Zakrzewska <Jeeves@aleksandrasair2.wireless.yale.internal>
2016-03-29 00:02:26 -0400

    finished stuff. added end stuff. fonts

--- dogenerator-master/app.rb
+++ dogenerator-master/app.rb
@@ -9,8 +9,8 @@ configure do
 end
 
 
-    $counter = 0
-    $question_num = 0
+$correct_counter = 0
+$question_num = 0
 
 #     @colors = ["blue", "red", "cyan", "fuchsia", "darkorchid", "yellow", "lime"]
 #     text = "wow"
@@ -18,34 +18,41 @@ end
 
 get "/" do
     text ||= ''
-
-    puts QLIST[$counter]
-    erb :"index.html", :locals => {:question => QLIST[$counter]}
+    if $question_num == QLIST.length()
+       erb :"end.html", :locals => {:score => $correct_counter}
+    else 
+        erb :"index.html", :locals => {:question => QLIST[$question_num], :question_num => $question_num}
+    end
 end
 
 post "/" do
-    #img = ImageList.new('pic.jpg')
-    answer = params[:inputtext].downcase
 
-    # check that text is answer
-    if answer == "a"
-        text = "correct"
-        $counter +=1
-    else 
-        text = "incorrect"
-    end
-    $question_num +=1
+        #img = ImageList.new('pic.jpg')
+        answer = params[:inputtext].downcase
+        incorrect_flag = false
 
-    # if yes, display happy if no display sad
+        # check that text is answer
+        #if answers.include?(answer)
+       # if ALIST[$question_num].include?(answer)
+       if ALIST[$question_num] == answer
+            text = "Correct"
+            $correct_counter +=1
+        else 
+            text = "Incorrect"
+            incorrect_flag = true
+        end
+        $question_num +=1
+
+        # if yes, display happy if no display sad
 
 
-    #result = gen_all(pnouns, adjs, singnouns)
-    # numwows = 9 / 3
-    # i=0
+        #result = gen_all(pnouns, adjs, singnouns)
+        # numwows = 9 / 3
+        # i=0
+            
         
-    
-    erb :"image.html", :locals => {:text => text, :colors => @colors}
-    #slim : "image.html", :text => {:text=> text, :result => result.sample}
+        erb :"image.html", :locals => {:text => text, :colors => @colors, :answer => ALIST[$question_num-1], :incorrect_flag => incorrect_flag}
+        #slim : "image.html", :text => {:text=> text, :result => result.sample}
 end
 
 
