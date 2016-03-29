895b1a77d69010ebabdcfa8ff05cd8f9737e39f1
Aleksandra Zakrzewska <Jeeves@Aleksandras-MacBook-Air-2.local>
2016-03-29 09:50:11 -0400

    added start page and saved score

--- dogenerator-master/app.rb
+++ dogenerator-master/app.rb
@@ -18,10 +18,19 @@ $question_num = 0
 
 get "/" do
     text ||= ''
-    if $question_num == QLIST.length()
-       erb :"end.html", :locals => {:score => $correct_counter}
+    if $question_num == 0
+        $question_num+=1
+         erb :"start.html", :locals => {}
+        
+     #    erb :"index.html", :locals => {:question => QLIST[$question_num-1], :question_num => $question_num}
+      #   $question_num+=1
+    elsif $question_num == QLIST.length()
+        open('scores.out', 'a') do |f|
+            f.puts "score is: " + $correct_counter.to_s + "\n"
+        end
+      erb :"end.html", :locals => {:score => $correct_counter}
     else 
-        erb :"index.html", :locals => {:question => QLIST[$question_num], :question_num => $question_num}
+       erb :"index.html", :locals => {:question => QLIST[$question_num-1], :question_num => $question_num}
     end
 end
 
@@ -34,14 +43,14 @@ post "/" do
         # check that text is answer
         #if answers.include?(answer)
        # if ALIST[$question_num].include?(answer)
-       if ALIST[$question_num] == answer
+       if ALIST[$question_num-1] == answer
             text = "Correct"
             $correct_counter +=1
         else 
             text = "Incorrect"
             incorrect_flag = true
         end
-        $question_num +=1
+
 
         # if yes, display happy if no display sad
 
@@ -50,9 +59,10 @@ post "/" do
         # numwows = 9 / 3
         # i=0
             
-        
-        erb :"image.html", :locals => {:text => text, :colors => @colors, :answer => ALIST[$question_num-1], :incorrect_flag => incorrect_flag}
+        $question_num +=1
+        erb :"image.html", :locals => {:text => text, :colors => @colors, :answer => ALIST[$question_num-2], :incorrect_flag => incorrect_flag}
         #slim : "image.html", :text => {:text=> text, :result => result.sample}
+         
 end
 
 
